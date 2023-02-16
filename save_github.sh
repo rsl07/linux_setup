0cce44b26aee670fc4ace8d4b65414e39165291a
Antoine Roussel <antoine.roussel@edf.fr>
2023-02-16 09:29:47 +0100

    quick_save

--- save_github.sh
+++ save_github.sh
@@ -13,19 +13,19 @@ cp $HOME/.zshrc zshrc.sh
 cp $HOME/.rc0 rc0.sh
 
 echo .zshrc and .rc0 from home loaded into linux config file 
-sleep 1
+echo 
 
 git add *
 git commit -m "quick_save"
 git push github_rsl07
 
-sleep 1
 
 # Subl User Config Save
 
-echo going for ST config
+echo
+echo going for ST config :
 
-cd $HOME/ST_USER
+cd $ST_USER
 git add *
 git commit -m "quick_save"
 git push github_rsl07
