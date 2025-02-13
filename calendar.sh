4ee8f59b0e10663c1f91ebab34e5275a5dcfcbec
Antoine ROUSSEL <antoine.roussel@edf.fr>
2025-02-14 00:33:06 +0100

    git_save_config.sh

--- calendar.sh
+++ calendar.sh
@@ -1,52 +1,71 @@
 #!/bin/bash
 
-
+echo ".pid_calendar : "
 id_calendar=`cat $HOME/.pid_calendar`
 echo $id_calendar
+echo
 
-name=$(xdotool getwindowname $id_calendar)
-echo $name
+echo "pid_calendar classname"
+class_calendar=$(xprop WM_CLASS -id $id_calendar )
+echo $class_calendar
+echo 
 
-if xdotool getwindowname $id_calendar && [ "$name" != "" ];
+
+if [[ $class_calendar == *"calendar"*"chrome"* ]];
 then
 
-	echo "Already Launched"
+	echo "chrome launched"
 
 	id_focus=$(xdotool getwindowfocus)
 
-	echo $id_focus
-	echo $id_calendar
-	
+
 	if [ "$id_focus" -eq "$id_calendar" ]
 	then
+
+	    echo "   calendar focus -> minimize"
 
 		xdotool windowminimize $id_calendar
 
 	else
 
-	    echo "chrome opened but not focus"
+	    echo "   calender not focus -> focus"
 	    
-		xdotool windowsize $id_calendar 100% 100%
-		xdotool windowactivate $id_calendar
+	    xdotool windowactivate $id_calendar
 
 	fi
-
+	
 else
-    echo "Calendar not opened"
+
+	echo "Not Launched, proceed to launch another app"
+
+	url1="https://outlook.office365.com/calendar/view/month"
+	url2="https://pgi-ssl.edf.fr/gta/OpenhrCore/logon.jsp?Domaine=null"
+	url3="https://www.notion.so/Ordo-5eaaab45039745c18e42e1a418a9e78f"
+
+
+	google-chrome --new-window $url1 $url2 $url3 & disown
+
+	# bring chrome main to the front ----------------------------------------
+
+	sleep 0.5
+
+	wmctrl -l | grep "Google Chrome" | sort -r  
+
+	last_calendar=$(wmctrl -l | grep "Google Chome" | sort -r | head -n 1 | cut -d " " -f 1) 
+
+
+	echo "last chrome main opened: "$last_calendar
     
-    
-   	url1="https://outlook.office.com/calendar/view/month"
+	wmctrl -i -a $last_calendar
 
-	google-chrome $url1 & disown
-
-	sleep 2
-
+	# --------------------------------------------------------------------------
+	      
 	export id_calendar=$(xdotool getwindowfocus)
 
-	rm $HOME/.pid_calendar
+	xdotool set_window --classname "calendar" $id_calendar
 
 	echo $id_calendar > $HOME/.pid_calendar
 
-
-
 fi
+
+
